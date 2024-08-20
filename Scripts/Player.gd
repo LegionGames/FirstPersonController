ce0268319ef1b6ed2c6a938fe3fe2d63f4829c75
Ryan Battler <ovatar@gmail.com>
2024-08-20 15:36:04 +0300

    FIX: Player movement when rotated at start

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -50,7 +50,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
